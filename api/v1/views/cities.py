a56a2da91c921c18706a71595463bfbcec3c7a10
abdelkaderbelkacemi13 <belkacemi.abdelkader54@gmail.com>
2024-08-05 09:13:07 -0700

    new test

--- api/v1/views/cities.py
+++
@@ -1,8 +0,0 @@
-#!/usr/bin/python3
-""""""
-from flask import abort, jsonify, make_response, request
-from models.state import State
-from models.city import City
-from api.v1.views import app_views
-
-
